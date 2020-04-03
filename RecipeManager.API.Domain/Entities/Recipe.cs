529082197e3693345297f6d21fbed506b45c3cb2
Tobin Stultiens <tobin.stultiens@gmail.com>
2020-04-03 21:57:53 +0200

    Added model validation

--- RecipeManager.API.Domain/Entities/Recipe.cs
+++ RecipeManager.API.Domain/Entities/Recipe.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RecipeManager.API.Domain.Entities
 {
@@ -7,11 +8,16 @@ namespace RecipeManager.API.Domain.Entities
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Ingredients are needed for a recipe")]
         public List<Ingredient> Ingredients { get; set; }
+        [Required(ErrorMessage = "Directions are needed to make the recipe properly")]
         public List<Direction> Directions { get; set; }
         public RecipeTime RecipeTime { get; set; }
+        [Url]
         public string ImgLink { get; set; }
+        [Url]
         public string VideoLink { get; set; }
         public DateTime CreationDateTime { get; set; }
         public DateTime LastUpdatedDateTime { get; set; }
